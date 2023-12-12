0b03357359a147f6546a12595bf5432e6367d04e
TedYettis34 <152925900+TedYettis34@users.noreply.github.com>
2023-12-12 11:28:59 -0600

    Add Pup wrangler

--- copyStuff.sh
+++ copyStuff.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-subtractAmount=306
-for i in {310..349}
+subtractAmount=346
+for i in {350..389}
 do
-    cp metadata/LadyApollo.json "metadata/$i.json"
+    cp metadata/PupWrangler.json "metadata/$i.json"
     sed -i'' -e "s/0/$(($i - $subtractAmount))/g" metadata/$i.json
 done
