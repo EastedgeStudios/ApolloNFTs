fd11fec656220a8653b4cecdcb95907456d25595
TedYettis34 <152925900+TedYettis34@users.noreply.github.com>
2023-12-12 11:16:06 -0600

    Updated script and added Apollo Knight

--- copyStuff.sh
+++ copyStuff.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+subtractAmount=106
 for i in {110..149}
 do
     cp metadata/ApolloKnight.json "metadata/$i.json"
+    sed -i'' -e "s/0/$(($i - $subtractAmount))/g" metadata/$i.json
 done
